eca7a11032468f604d5982df375683e38441a68c
Valber Jr <valbercj@gmail.com>
2020-09-12 11:40:10 -0300

    Node Challenge Fundamentals!

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,7 +14,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
+
+    if (!["income", "outcome"].includes(type)){
+      throw new Error('It is not a valid transaction!');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value){
+      throw new Error('You do not have enouth cash!');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
     // TODO
   }
 }
